4af900b84336a28ce5125bbe9f868180ea74e5aa
harrycode <pacboy2004@gmail.com>
2017-10-28 08:43:55 +1000

    more maths stuff i guess?

--- framework/maths/general.js
+++ framework/maths/general.js
@@ -2,6 +2,26 @@ function add(n1, n2) {
   return n1 + n2;
 }
 
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+function square(n1) {
+  return n1 * n1;
+}
+
+function half(n1) {
+  return n1 / 2;
+}
+
 // not sure about this one...
 function arrayTotal(array) {
   array.reduce(add, 0);
