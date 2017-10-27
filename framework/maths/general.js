20ac6b61320ffd5f97e3e40a9a76278919f1fd0e
Harry <pacboy2004@gmail.com>
2017-10-27 12:22:53 +1000

    Update general.js

--- framework/maths/general.js
+++ framework/maths/general.js
@@ -1,1 +1,9 @@
-// need syntax highlighting 1st
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+// not sure about this one...
+function arrayTotal(array) {
+  array.reduce(add, 0);
+  add(a, b);
+}
