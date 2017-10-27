dd134f5255c4a28f2a5a502da9ea56d17fc45d06
Harry <pacboy2004@gmail.com>
2017-10-27 12:12:11 +1000

    Update document.js

--- framework/main/document.js
+++ framework/main/document.js
@@ -2,3 +2,28 @@ function changeElement(id, newtext) {
   var element = document.getElementById(id);
   element.innerHTML = newtext;
 }
+
+function newElement(element) {
+  document.createElement(element);
+}
+
+function attachElement(element) {
+  document.appendChild(element);
+}
+
+function detachElement(element) {
+  document.removeChild(element):
+}
+
+function reattachElement(element) {
+  document.replaceChild(element);
+}
+
+function codifyElement(id, code) {
+  document.getElementById(id).onclick = function(){code}
+}
+
+function addAttribute(elementid, attr, value) {
+  var element = document.getElementById(elementid);
+  element.setAttribute(attr, value);
+}
